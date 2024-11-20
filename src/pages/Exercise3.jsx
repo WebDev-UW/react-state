9ff1a562d142f51275fd912cd760b2c0217be0ed
kavakian <38766915+kegdotcom@users.noreply.github.com>
2024-11-19 19:03:09 -0600

    added e3 todo

--- src/pages/Exercise3.jsx
+++ src/pages/Exercise3.jsx
@@ -6,6 +6,12 @@ export default function Exercise3() {
   const [username, setUsername] = useState("my user");
   const [password, setPassword] = useState("my password");
 
+  // TODO: use Context and Reducers to make this code less repetitive and do the following functionality
+  // 1. should be able to access the username and password in all children
+  // 2. should be able to change username and password in all children
+  // 3. no props should be passed to child components
+
+
   return (
     <div>
       <div>
